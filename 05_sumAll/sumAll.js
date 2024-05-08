1f3a8a42bace30448cb465089a779dc6437d10e8
Kunwar Singh <dev@kunwar.me>
2024-05-09 00:01:28 +0530

    Passed all tests in 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (startRange, endRange) {
+  if (!Number.isInteger(startRange) || !Number.isInteger(endRange))
+    return 'ERROR';
+  if (startRange < 0 || endRange < 0) return 'ERROR';
+  if (startRange > endRange) {
+    const temp = startRange;
+    startRange = endRange;
+    endRange = temp;
+  }
+  let result = 0;
 
+  while (startRange <= endRange) {
+    result += startRange;
+    startRange++;
+  }
+  return result;
 };
 
 // Do not edit below this line
