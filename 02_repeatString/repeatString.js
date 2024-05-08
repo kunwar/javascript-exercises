51e41097546456d7b381b7a33c7e18af524a77a9
Kunwar Singh <dev@kunwar.me>
2024-05-08 23:39:12 +0530

    Passed all tests in 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, numOfRepititions) {
+  if (numOfRepititions < 0) return 'ERROR';
+  let result = '';
+  for (let i = 0; i < numOfRepititions; i++) {
+    result += str;
+  }
+  return result;
 };
 
 // Do not edit below this line
